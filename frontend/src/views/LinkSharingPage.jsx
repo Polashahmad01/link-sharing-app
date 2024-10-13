730dc183d0be641ef9a799034ce597964681a145
Polashahmad01 <dev.polashahmad@gmail.com>
2024-10-14 01:14:54 +0600

    Add user data fetching on LinkSharingPage

--- frontend/src/views/LinkSharingPage.jsx
+++ frontend/src/views/LinkSharingPage.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 import LinkShowCase from "../components/LinkShowCase";
+import { getCurrentUserMutation } from "../services/auth.service";
 
 export default function LinkSharingPage() {
+  const params = useParams();
+  const memoizedParams = useMemo(() => params, [params]);
+  const { mutate, data, isPending } = useMutation({
+    mutationKey: ["get-current-user"],
+    mutationFn: getCurrentUserMutation,
+  });
+  console.log(params);
+
+  useEffect(() => {
+    mutate({ _id: memoizedParams.userId });
+  }, [memoizedParams, mutate]);
+
   return (
     <div className="-mt-16">
       <LinkShowCase />
